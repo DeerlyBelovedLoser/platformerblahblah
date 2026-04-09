341884ca8e4846f4eb1bf883de2880058d4d7ff9
DeerlyBelovedLoser <deerlybelovedloser@gmail.com>
2026-04-09 15:38:21 -0500

    peepeepoopoo

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,14 +6,24 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    bool isFacingRight = true; 
     public float moveSpeed = 5f;
-
     float horizontalMovement;
     public float jumpPower = 10f;
+    public int maxJumps = 2;
+    int jumpsRemaining; 
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
-
+    bool isGrounded;
+    public float baseGravity = 2f;
+    public float maxFallSpeed = 18f;
+    public float fallSpeedMultiplier = 2f;
+    public Transform wallCheckPos;
+    public Vector2 wallCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask wallLayer;
+    public float wallSlideSpeed = 2f;
+    bool isWallSliding;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +35,10 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        GroundCheck();
+        Gravity();
+        Flip();
+        wallSlide();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -34,35 +48,85 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(isGrounded())
+        if(jumpsRemaining > 0)
         {
         if(context.performed)
         {
             //hold down
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            jumpsRemaining--;
         }
         else if (context.canceled)
         {
             //tap 
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            jumpsRemaining--;
         }
         }
     }
 
-    private bool isGrounded()
+    private void GroundCheck()
     {
         if(Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
         {
-            return true;
+            jumpsRemaining = maxJumps;
+            isGrounded = true;
         }
-        return false;
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(wallCheckPos.position, wallCheckSize);
     }
 
+    private void Gravity()
+    {
+        if(rb.linearVelocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier; //who up falling faster over time
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private bool wallCheck()
+    {
+        return Physics2D.OverlapBox(wallCheckPos.position, wallCheckSize, 0, wallLayer);
+    }
+
+    private void wallSlide()
+    {
+        //Not gorunded/on a wall/movement ! = 0
+        if(!isGrounded & wallCheck() & horizontalMovement != 0)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -wallSlideSpeed));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
 
 }
